d81a727d352c93c98ce9c5151f92188c049585fe
Jordan Gardner <jordanthomasg@gmail.com>
2024-09-16 22:26:12 -0600

    Initial Commit

---
+++ Hearts/HeartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartsApp.swift
+//  Hearts
+//
+//  Created by Jordan Gardner on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
