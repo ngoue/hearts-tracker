1d056154645de46ca6ea25e9d0d6758f2e2034ad
Jordan Gardner <jordanthomasg@gmail.com>
2024-10-06 11:00:43 -0600

    Add Firebase

--- Hearts/HeartsApp.swift
+++ Hearts/HeartsApp.swift
@@ -5,10 +5,20 @@
 //  Created by Jordan Gardner on 9/16/24.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct HeartsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
